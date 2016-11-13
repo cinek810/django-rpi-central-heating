9ee85d093b30f824c4b62b604ffd63ddcc26719c
Marcin Stolarek <stolarek.marcin@gmail.com>
2016-11-13 13:57:22 +0100

    validate period hours, present sorted list in view

--- sterowaniePokoi/models.py
+++ sterowaniePokoi/models.py
@@ -15,10 +15,10 @@ class Sensor(models.Model):
 	outPin=models.IntegerField(default=-1)
 	
 	def __str__(self):
-		return self.name
+		return self.name+" ID:"+self.sensorId
 	def getTemp(self):
 		owproxy=pyownet.protocol.proxy(host="127.0.0.1", port=4304)
-		return round(float(owproxy.read('/'+self.sensorId+'/temperature')),2)
+		return round(float(owproxy.read('/'+self.sensorId+'/temperature')),1)
 	def getSetTemp(self):
 		currentTime=datetime.now()
 		setTemp=self.setTemp
@@ -36,6 +36,10 @@ class Sensor(models.Model):
 			return 0
 		else:
 			return 2
+	def periodsByStart(self):
+		return self.period_set.order_by('startHour')
+
+		
 		
 		
 
@@ -46,3 +50,9 @@ class Period(models.Model):
 	temp=models.IntegerField()
 	def __str__(self):
 		return self.sensor.name+" Start:"+str(self.startHour)+" End:"+str(self.endHour)+" Temp:"+str(self.temp)
+	def clean(self):
+		if self.endHour <= self.startHour:
+			raise ValueError("End >= Start")
+		for period in self.sensor.period_set.all():
+			if (self.startHour>=period.startHour and self.startHour<period.endHour ) or ( self.endHour <= period.endHour and self.endHour>period.startHour):
+				raise ValueError("Overlaps with different period")
